1f878cc2feb987400e5fc2a9683b4b41c2d8f80f
Rahul Mahale <rahulmahale@gmail.com>
2020-09-10 23:19:25 +0530

    Add files via upload

---
+++ PythonAssignment2/A2_pattern.py
@@ -0,0 +1,13 @@
+# 1. Create the below pattern using nested for loop in Python.
+
+rows = 5
+for i in range(0, rows):
+    for j in range(0, i + 1):
+        print("*", end=' ')
+    print("\r")
+
+
+for i in range(rows, 0, -1):
+    for j in range(0, i - 1):
+        print("*", end=' ')
+    print("\r")
---
+++ PythonAssignment2/A2_reverse.py
@@ -0,0 +1,10 @@
+#2. Write a Python program to reverse a word after accepting the input from the user.
+##Input word: c
+#Output: norueni
+
+
+str1 = input(" Enter word ")
+
+str2 = str1[-1::-1]
+
+print(str2)
